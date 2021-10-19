ee2ede9b559213b0c30a8e8fb3441844ef0a24bc
Inna <inna.kachinskaya13@gmail.com>
2021-10-19 18:29:56 +0300

    Refactor. Add Counter.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,8 @@
-import { connect } from "react-redux";
-import * as actionCreators from "./actions/index";
-function App(props) {
-  console.log(props);
-  const { count, step, incrementAction, decrementAction, setStepAction } =
-    props;
+import React from "react";
+import Counter from "./components/Counter/counter";
 
-  const changeStep = ({ target: { value } }) => setStepAction(Number(value));
-
-  return (
-    <div>
-      <h1>Counter is: {count} </h1>
-      <input vallue={step} onChange={changeStep} />
-      <button onClick={decrementAction}>Отнять</button>
-      <button onClick={incrementAction}>Добавить</button>
-    </div>
-  );
+function App() {
+  return  <Counter />;
 }
 
-//принимает состояние, возвращает состояние, к-рое д.б. подключено к этому компоненту
-//можно отсечь ненужное, взять только нужное
-function mapStateToProps(state) {
-  return state;
-}
-
-
-function mapDispatchToProps(dispatch) {
-  return {
-    incrementAction: () => dispatch(actionCreators.increment()),
-    decrementAction: () => dispatch(actionCreators.decrement()),
-    setStepAction: (newStep) => dispatch(actionCreators.setStep(newStep)),
-  };
-}
-//с помощью каррирования
-export default connect(mapStateToProps, mapDispatchToProps)(App);
-
-/* 
-connect соединяет компонент с хранилищем 
-принимает 4 разных, необязательных параметра
-mapStateToProps - принимает состояние, возвращает состояние, к-рое д.б. подключено к этому компоненту
-*/
+export default App;
---
+++ src/components/Counter/counter.jsx
@@ -0,0 +1,40 @@
+import { connect } from "react-redux";
+import * as actionCreators from "./../../actions";
+function App(props) {
+  console.log(props);
+  const { count, step, incrementAction, decrementAction, setStepAction } =
+    props;
+
+  const changeStep = ({ target: { value } }) => setStepAction(Number(value));
+
+  return (
+    <div>
+      <h1>Counter is: {count} </h1>
+      <input vallue={step} onChange={changeStep} />
+      <button onClick={decrementAction}>Отнять</button>
+      <button onClick={incrementAction}>Добавить</button>
+    </div>
+  );
+}
+
+//принимает состояние, возвращает состояние, к-рое д.б. подключено к этому компоненту
+//можно отсечь ненужное, взять только нужное
+function mapStateToProps(state) {
+  return state;
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    incrementAction: () => dispatch(actionCreators.increment()),
+    decrementAction: () => dispatch(actionCreators.decrement()),
+    setStepAction: (newStep) => dispatch(actionCreators.setStep(newStep)),
+  };
+}
+//с помощью каррирования
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+/* 
+connect соединяет компонент с хранилищем 
+принимает 4 разных, необязательных параметра
+mapStateToProps - принимает состояние, возвращает состояние, к-рое д.б. подключено к этому компоненту
+*/
