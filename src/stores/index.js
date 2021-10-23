eb933a13f3383312d0c6743e173a4175bea75231
Inna <inna.kachinskaya13@gmail.com>
2021-10-23 12:35:39 +0300

    Add redux-devtools

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,11 +1,15 @@
 import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducer from "../reducers";
 import rootSaga from "../sagas";
 const sagaMiddleware = createSagaMiddleware();
 
 //создаем хранилище для всех состояний
 //менять можно только dispatch(action)
-const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
 sagaMiddleware.run(rootSaga);
 export default store;
