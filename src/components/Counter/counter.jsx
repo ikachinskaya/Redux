38a103bb67e173025e9a0431ee9df2c3e36d9afa
Inna <inna.kachinskaya13@gmail.com>
2021-10-23 13:04:36 +0300

    Hooks useSelector, useDispatch

--- src/components/Counter/counter.jsx
+++ src/components/Counter/counter.jsx
@@ -1,9 +1,27 @@
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import * as actionCreators from "./../../actions/counterCreators";
-function App(props) {
+
+function Counter(props) {
   console.log(props);
-  const { count, step, incrementAction, decrementAction, setStepAction } =
-    props;
+  // const { count, step, incrementAction, decrementAction, setStepAction } =
+  //   props;
+
+  //useSelector - Позволяет извлекать данные из состояния хранилища Redux с помощью функции селектора.
+  //1 параметр - селектор - функция, которая говорит, что достать из state
+  const { step, count } = useSelector((state) => {
+    const {
+      counter: { step, count },
+    } = state;
+    return { step, count };
+  });
+
+  //useDispatch - возвращает ссылку на функцию отправки из хранилища Redux (т.е. dispatch)
+  //Вы можете использовать его для отправки действий по мере необходимости.
+  const dispatch = useDispatch();
+
+  const incrementAction = () => dispatch(actionCreators.increment());
+  const decrementAction = () => dispatch(actionCreators.decrement());
+  const setStepAction = (newStep) => dispatch(actionCreators.setStep(newStep));
 
   const changeStep = ({ target: { value } }) => setStepAction(Number(value));
 
@@ -19,22 +37,22 @@ function App(props) {
 
 //принимает состояние, возвращает состояние, к-рое д.б. подключено к этому компоненту
 //можно отсечь ненужное, взять только нужное
-function mapStateToProps(state) {
-  const {
-    counter: { step, count },
-  } = state;
-  return { step, count };
-}
+// function mapStateToProps(state) {
+//   const {
+//     counter: { step, count },
+//   } = state;
+//   return { step, count };
+// }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    incrementAction: () => dispatch(actionCreators.increment()),
-    decrementAction: () => dispatch(actionCreators.decrement()),
-    setStepAction: (newStep) => dispatch(actionCreators.setStep(newStep)),
-  };
-}
-//с помощью каррирования
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     incrementAction: () => dispatch(actionCreators.increment()),
+//     decrementAction: () => dispatch(actionCreators.decrement()),
+//     setStepAction: (newStep) => dispatch(actionCreators.setStep(newStep)),
+//   };
+// }
+
+export default Counter;
 
 /* 
 connect соединяет компонент с хранилищем 
